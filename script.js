3ad8ff1e941cd07815de88c8b7726b3c4871dba1
Brian Wong <brianw7218@gmail.com>
2022-08-09 21:58:15 -0700

    Reorganize the layout of code to flow logically

--- script.js
+++ script.js
@@ -1,3 +1,69 @@
+let playerPoints = 0;
+let computerPoints = 0;
+
+let rock = document.querySelector("#rock");
+rock.addEventListener("click", () => {
+    showResults("Rock");
+    showPoints();
+    checkPoints();
+});
+
+let paper = document.querySelector("#paper");
+paper.addEventListener("click", () => {
+    showResults("Paper");
+    showPoints();
+    checkPoints();
+});
+
+let scissors = document.querySelector("#scissors");
+scissors.addEventListener("click", () => {
+    showResults("Scissors");
+    showPoints();
+    checkPoints();
+});
+
+const showResults = (choice) => {
+    let message = document.querySelector(".message");
+    let computerSign = document.querySelector(".computer");
+    let results;
+    let computerChoice;
+    computerChoice = getComputerChoice();
+    results = playRound(choice, computerChoice);
+    computerSign.textContent = `The computer picked ${computerChoice}!`;
+    message.textContent = results;
+    pickWinner(results);
+}
+
+const showPoints = () => {
+    let playerDisplay = document.querySelector(".player-points");
+    let computerDisplay = document.querySelector(".computer-points");
+    if (playerPoints === 1) {
+        playerDisplay.textContent = `You currently have 1 point.`;
+    } else {
+        playerDisplay.textContent = `You currently have ${playerPoints} points.`;
+    }
+    if (computerPoints === 1) {
+        computerDisplay.textContent = `The computer currently has 1 point.`;
+    } else {
+        computerDisplay.textContent = `The computer currently has ${computerPoints} points.`;
+    }
+}
+
+const checkPoints = () => {
+    if (playerPoints === 5 || computerPoints === 5) {
+        let gameOver = document.querySelector(".game-over");
+        if (playerPoints === 5) {
+            gameOver.textContent = "Congratulations, you won!";
+            playAgain();
+            disableButtons();
+        } else if (computerPoints === 5) {
+            gameOver.textContent = "You lose, try again!";
+            playAgain();
+            disableButtons();
+        }
+    }
+}
+
 function getComputerChoice() {
     let choice;
     let randomValue = Math.floor((Math.random() * 3) + 1);
@@ -29,65 +95,12 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-let playerPoints = 0;
-let computerPoints = 0;
-
 const pickWinner = (message) => {
     if (message.substring(4,7) === "Win") {
         playerPoints++;
     } else if (message.substring(4,8) === "Lose") {
         computerPoints++;
     }
-}
-
-const showResults = (choice) => {
-    let message = document.querySelector(".message");
-    let computerSign = document.querySelector(".computer");
-    let results;
-    let computerChoice;
-    computerChoice = getComputerChoice();
-    results = playRound(choice, computerChoice);
-    computerSign.textContent = `The computer picked ${computerChoice}!`;
-    message.textContent = results;
-    pickWinner(results);
-}
-
-const showPoints = () => {
-    let playerDisplay = document.querySelector(".player-points");
-    let computerDisplay = document.querySelector(".computer-points");
-    if (playerPoints === 1) {
-        playerDisplay.textContent = `You currently have 1 point.`;
-    } else {
-        playerDisplay.textContent = `You currently have ${playerPoints} points.`;
-    }
-    if (computerPoints === 1) {
-        computerDisplay.textContent = `The computer currently has 1 point.`;
-    } else {
-        computerDisplay.textContent = `The computer currently has ${computerPoints} points.`;
-    }
-}
-
-const removeText = () => {
-    let divs = document.querySelectorAll("div");
-    divs.forEach((div) => {
-        div.textContent = "";
-    })
-}
-
-const enableButtons = () => {
-    let buttons = document.querySelectorAll(".option");
-    buttons.forEach((button) => {
-        button.disabled = false;
-    })
-}
-
-const resetGame = () => {
-    playerPoints = 0;
-    computerPoints = 0;
-    let playAgain = document.querySelector("#play-again");
-    playAgain.remove();
-    removeText();
-    enableButtons();
 }
 
 const playAgain = () => {
@@ -109,38 +122,25 @@ const disableButtons = () => {
     })
 }
 
-const checkPoints = () => {
-    if (playerPoints === 5 || computerPoints === 5) {
-        let gameOver = document.querySelector(".game-over");
-        if (playerPoints === 5) {
-            gameOver.textContent = "Congratulations, you won!";
-            playAgain();
-            disableButtons();
-        } else if (computerPoints === 5) {
-            gameOver.textContent = "You lose, try again!";
-            playAgain();
-            disableButtons();
-        }
-    }
+const resetGame = () => {
+    playerPoints = 0;
+    computerPoints = 0;
+    let playAgain = document.querySelector("#play-again");
+    playAgain.remove();
+    removeText();
+    enableButtons();
 }
 
-let rock = document.querySelector("#rock");
-rock.addEventListener("click", () => {
-    showResults("Rock");
-    showPoints();
-    checkPoints();
-});
+const removeText = () => {
+    let divs = document.querySelectorAll("div");
+    divs.forEach((div) => {
+        div.textContent = "";
+    })
+}
 
-let paper = document.querySelector("#paper");
-paper.addEventListener("click", () => {
-    showResults("Paper");
-    showPoints();
-    checkPoints();
-});
-
-let scissors = document.querySelector("#scissors");
-scissors.addEventListener("click", () => {
-    showResults("Scissors");
-    showPoints();
-    checkPoints();
-});
+const enableButtons = () => {
+    let buttons = document.querySelectorAll(".option");
+    buttons.forEach((button) => {
+        button.disabled = false;
+    })
+}
