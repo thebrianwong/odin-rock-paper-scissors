a67d2f84f4af22c0ede8ba6f2c356dd15487a9c4
Brian Wong <brianw7218@gmail.com>
2022-08-09 21:52:31 -0700

    Remove code that ran the game in the console

--- script.js
+++ script.js
@@ -29,67 +29,6 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-// Checks if player input is a valid option in the game. If not, the player will be prompted again until they enter a valid option. 
-function inputValidation(playerInput) {
-    if (playerInput === "Rock" || playerInput === "Paper" || playerInput === "Scissors") {
-        return true;
-    }
-    return false;
-}
-
-// Alters input so that the first letter is uppercase and the rest of the word is lowercase.
-function fixCaseSensitivity(string) {
-    let firstLetterUpper = string.slice(0,1).toUpperCase();
-    let restOfWordLower = string.slice(1, string.length).toLowerCase();
-    string = firstLetterUpper + restOfWordLower;
-    return string;
-}
-
-function game() {
-    let playerPoints = 0;
-    let computerPoints = 0;
-    // i starts at 1 so that the displayed Round Number starts at 1, not 0.
-    for (let i = 1; i < 6; i++) {
-        console.log(`Round ${i}`);
-        // Do while loop that asks player for input until they enter a valid option.
-        do {
-            var playerSelection = prompt("Rock, Paper, or Scissors?");
-            playerSelection = fixCaseSensitivity(playerSelection);
-        } while (inputValidation(playerSelection) === false);
-        let computerSelection = getComputerChoice();
-        let results = playRound(playerSelection, computerSelection);
-        console.log(`You picked ${playerSelection}!`);
-        console.log(results);
-        // Looks at the results message to see if the player or the computer won and gives a point accordingly.
-        /* If I had to make any further changes, I would change this logic so that it isn't so weirdly reliant on looking for a specific word in a specific range of 
-        characters specific string. Instead, I would change playRound() to return "Win", "Lose", or "Tie" instead of full strings. To mimic the announcement messages 
-        of round results that currently exist, I would use the new return values of playRound() and the playerSelection and the computerSelection in an if statement 
-        to print the announcement messages. An issue there is that "Scissors" is plural whereas "Rock" and "Paper" are singular, so there is some grammar issues 
-        there. */
-        if (results.substring(4,7) === "Win") {
-            playerPoints++;
-        } else if (results.substring(4,8) === "Lose") {
-            computerPoints++;
-        }
-    }
-    console.log("Final Results");
-    if (playerPoints > computerPoints) {
-        console.log("You Win!");
-        console.log(`Player Points: ${playerPoints}`);
-        console.log(`Computer Points: ${computerPoints}`);
-    } else if (computerPoints > playerPoints) {
-        console.log("You Lose!");
-        console.log(`Player Points: ${playerPoints}`);
-        console.log(`Computer Points: ${computerPoints}`);
-    } else {
-        console.log("It's a Tie!");
-        console.log(`Player Points: ${playerPoints}`);
-        console.log(`Computer Points: ${computerPoints}`);
-    }
-}
-
-//game();
-
 let playerPoints = 0;
 let computerPoints = 0;
 
@@ -116,7 +55,6 @@ const showResults = (choice) => {
 const showPoints = () => {
     let playerDisplay = document.querySelector(".player-points");
     let computerDisplay = document.querySelector(".computer-points");
-    // Later: add if statement so that these messages say "point" if player or computer only have 1 point
     if (playerPoints === 1) {
         playerDisplay.textContent = `You currently have 1 point.`;
     } else {
